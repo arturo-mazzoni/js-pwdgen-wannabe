bb3d3f2b595a73af24ec4008d84e4f8a63023a38
arturo <arturomazzoni@gmail.com>
2020-12-11 17:51:58 +0100

    password generator

---
+++ script.js
@@ -0,0 +1,14 @@
+var nome, cognome, colore;
+
+// nome = prompt('Inserisci il tuo nome');
+// cognome = prompt('Inserisci il tuo cognome');
+// colore = prmpt('Inserisci il tuo colore preferito');
+
+
+nome = prompt('Inserisci il tuo nome');
+cognome = prompt('Inserisci il tuo cognome');
+colore = prompt('Inserisci il tuo colore preferito');
+frase = nome + cognome + colore
+
+
+document.getElementById('password').innerHTML = frase + '20'
